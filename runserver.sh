3146d65a158e3999d774b6d27619ca545084b5d0
HollowRider <michaelkinseywilliams@gmail.com>
2024-12-03 19:20:45 +0000

    Fixing the SH file.

--- runserver.sh
+++ runserver.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# This is a bash script designed to run a Minecraft 1.20.1 server on an Arch Linux Machine. It comes packaged with the "Minecraft RCon Client", made by "stefanodriussi". (https://github.com/stefanodriussi/minecraft-remote-controller)
+# This is a bash script designed to run a Minecraft 1.20.1 server on an Arch Linux Machine.
 
 MIN_MEMORY=4G
 MAX_MEMORY=10G
 
-while true
-	git pull
-	./mcsleepingserverstarter.sh
-	sleep 10
-endwhile
+while true; do
+    git pull
+    ./mcsleepingserverstarter.sh
+    sleep 10
+done
