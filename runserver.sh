0d53813865ff030d3556c6ac79889e2d0444e2f4
Michael.K.Williams <michaelkinseywilliams@gmail.com>
2024-12-03 19:12:32 +0000

    aa

--- runserver.sh
+++ runserver.sh
@@ -3,18 +3,9 @@
 
 MIN_MEMORY=4G
 MAX_MEMORY=10G
-JAR_FILE=minecraft_1-20-1_Server.jar
-DOWNLOAD_URL=https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar
 
-if [ ! -f "$JAR_FILE" ]; then
-    echo "$JAR_FILE not found, downloading..."
-    curl -o "$JAR_FILE" "$DOWNLOAD_URL"
-fi
-
-echo "Starting Minecraft server..."
-echo "Minimum Memory: $MIN_MEMORY"
-echo "Maximum Memory: $MAX_MEMORY"
-echo "Jar File: $JAR_FILE"
-echo
-
-java -Xms$MIN_MEMORY -Xmx$MAX_MEMORY -jar $JAR_FILE nogui
+while true
+	git pull
+	./mcsleepingserverstarter.sh
+	sleep 10
+endwhile
