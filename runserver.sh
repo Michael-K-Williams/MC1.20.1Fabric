a1c5e7e605afb77d6b7e0eaa5a3402a591c742f8
HollowRider <michaelkinseywilliams@gmail.com>
2024-12-03 19:22:21 +0000

    Install This

--- runserver.sh
+++ runserver.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # This is a bash script designed to run a Minecraft 1.20.1 server on an Arch Linux Machine.
 
-MIN_MEMORY=4G
-MAX_MEMORY=10G
-
 while true; do
     git pull
-    ./mcsleepingserverstarter.sh
+    ./mcsleepingserverstarter-linux-x64
     sleep 10
 done
